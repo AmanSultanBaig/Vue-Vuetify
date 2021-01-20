b7aa165dd1c390b94b704d085f15bbf0a8be287d
Aman Sultan Baig <amansultanbaig@gmail.com>
2021-01-21 01:05:36 +0500

    header design and component added to project

---
+++ vue-vuetify/src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-app-bar color="deep-purple accent-4" dark>
+      <v-toolbar-title>Vue Vuetify</v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped>
+
+
+</style>
---
+++ vue-vuetify/src/pages/TodoList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Todo List</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoList"
+};
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+</style>
