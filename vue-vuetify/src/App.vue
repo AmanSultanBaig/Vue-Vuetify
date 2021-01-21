d1abed292f68a7892049f689bcbc5ecbcd1ea3cc
Aman Sultan Baig <amansultanbaig@gmail.com>
2021-01-21 16:24:18 +0500

    todolist component is designed with vuetify datatable

--- vue-vuetify/src/App.vue
+++ vue-vuetify/src/App.vue
@@ -3,8 +3,6 @@
     <v-main>
       <Header />
 
-      <AddTodo />
-
       <TodoList />
 
       <Footer />
@@ -16,14 +14,12 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-import AddTodo from "./pages/AddTodo";
 import TodoList from "./pages/TodoList";
 
 export default {
   name: "App",
 
   components: {
-    AddTodo,
     TodoList,
     Header,
     Footer
--- vue-vuetify/src/components/Header.vue
+++ vue-vuetify/src/components/Header.vue
@@ -15,5 +15,4 @@ export default {
 
 <style scoped>
 
-
 </style>
